3cb57cec53a011204d3c9b4ac539baaea1fcf548
ABlack-git <marfunner@gmail.com>
2018-05-11 02:51:59 +0300

    Bug fix

--- demos/demos.py
+++ demos/demos.py
@@ -5,19 +5,19 @@ import os
 
 
 def pretrained_model_test():
-    path = ''
+    path = 'E:\Andrew\Dataset\Testing set\Images'
     imgs = utils.list_of_images(path)
     params = None
     img_size = (720, 480)
     grid_size = (36, 24)
     net = YoloV01(grid_size, img_size, params, restore=True)
-    meta_path = ''
-    weight_path = ''
+    meta_path = "E:\Andrew\Ann_models\pretrain_model_1_0\weights\model-30600.meta"
+    weight_path = "E:\Andrew\Ann_models\pretrain_model_1_0\weights\model-30600"
     net.restore(weight_path, meta_path)
     for img_path in imgs:
         img = cv2.imread(os.path.join(path, img_path))
-        img = utils.resize_img(img, img_size[0], img_size[1])
-        preds = net.get_predictions(img)
+        img = utils.resize_img(img, img_size[1], img_size[0])
+        preds = net.get_predictions([img])
         coords = net.predictions_to_cells(preds)
         utils.draw_centers(img, coords)
         utils.draw_grid(grid_size)

3f7a7387a29b18008b6afa5379f4375448ab3de3
Andrii Zakharchenko <zakhaand@fel.cvut.cz>
2018-06-25 22:33:10 +0200

    fix in utils draw_bbox

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import os
 
 
 def draw_bbox(data, img):
+    if not data:
+        return None
     for rect in data:
         cv2.rectangle(img, (rect[0], rect[1]), (rect[2], rect[3]), (0, 139, 0), 2)
 
